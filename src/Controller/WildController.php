61a39a46d41596949e46c8b282db7fc7d62d11e8
heldeenn <amandine.ln7@gmail.com>
2020-05-12 11:33:56 +0200

    Quest 5 : ajout show controller et vue

---
+++ src/Controller/WildController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class WildController extends AbstractController
+{
+    /**
+     * @Route("/wild", name="wild_index")
+     * @return Response
+     */
+    public function index() :Response
+    {
+        return $this->render('wild/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *     requirements={"slug"="[a-z0-9-]+"},
+     *     name="wild_show")
+     * @param string $slug
+     * @return Response
+     */
+
+    public function show(string $slug): Response
+    {
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
+    }
+}
